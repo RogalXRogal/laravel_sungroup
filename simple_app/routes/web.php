b390d88244b86b353b21f01688b621a0f92c65be
Kamil <K.R.P.T1142@gmail.com>
2022-07-06 19:48:39 +0200

    Add routing

--- simple_app/routes/web.php
+++ simple_app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index']);
 
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/show_db', [PageController::class, 'show']);
+
+Route::post('/', [PageController::class, 'store']);
