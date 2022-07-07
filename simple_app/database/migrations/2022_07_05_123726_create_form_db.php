0c739bd654dcbe577e47692a2b01b1f62d2f684b
Kamil <K.R.P.T1142@gmail.com>
2022-07-07 18:46:16 +0200

    Table migration

--- simple_app/database/migrations/2022_07_05_123726_create_form_db.php
+++ simple_app/database/migrations/2022_07_05_123726_create_form_db.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('form_db', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('surrname');
+            $table->string('surname');
             $table->string('email');
             $table->integer('number');
             $table->string('message');
