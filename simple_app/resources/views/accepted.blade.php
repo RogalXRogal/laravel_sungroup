fb743365067f74cc2fbc126d8a6bfcecb971c5fa
Kamil <K.R.P.T1142@gmail.com>
2022-07-07 18:42:21 +0200

    Accept query blade

---
+++ simple_app/resources/views/accepted.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/accept.css">
+    <title>Accepted</title>
+</head>
+<body>
+    <div>
+        <img src="photos/thumb_up.png" alt="thumb_up">
+        <h1>Dane zaakceptowane</h1>
+        <br>
+        <a href="/">Wróc do strony głównej</a>
+    </div>
+</body>
+</html>
